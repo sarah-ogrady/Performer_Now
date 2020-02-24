97a55075074f88a427146ac9dd2282f43bfee35c
Gareth Cary <gareth@caryfamily.co.uk>
2020-02-24 17:08:51 +0000

    index_controller_view

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -1,4 +1,5 @@
 class PerformersController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index]
   def index
     @performers = Performer.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'performers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :performers, only: %i[index show] do
     resources :bookings, only: %i[new create]
