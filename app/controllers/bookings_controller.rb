75f9460d350eb07c057660691c7588676990a331
Jake Marshall <jakemarshall27@icloud.com>
2020-02-24 16:34:11 +0000

    Add create to bookings controller. Add user_id key to reviews. Remove review_id key from bookings.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @performer = Performer.find(params[:performer_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @performer = Performer.find(params[:performer_id])
+    @booking.performer = performer
+    @booking.user = current_user
+    @booking.total_price = performer.hourly_rate * @booking.total_hours
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:total_hours, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :performers, only: %i[index show] do
-    resources :bookings, only: %i[create] do
-      resources :reviews, only: %i[new create]
-    end
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: [] do
+    resources :reviews, only: %i[new create]
   end
 end
