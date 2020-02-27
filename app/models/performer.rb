1b864a329dcc8ae5aa26acb62e07723d8c2e1b8e
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-02-27 17:14:51 +0000

    finished booking page

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -3,4 +3,8 @@ class Performer < ApplicationRecord
   has_many :users, through: :bookings
   has_many :reviews, through: :bookings
   has_one_attached :photo
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
